24d22edef2a8fcd36c8402d774d6ff7df6c07c09
Nayef <n.alhaidari@hotmail.com>
2019-08-07 10:22:06 -0700

    adding presenttation file and validation graph

--- Functions.py
+++ Functions.py
@@ -45,3 +45,16 @@ def categrizer(row):
         counter +=1
         return np.nan
     return counter
+
+def print_confusion_matrix(confusion_matrix, class_names, figsize = (9,5), fontsize=18):
+    df_cm = pd.DataFrame(confusion_matrix, index=class_names, columns=class_names, )
+    fig = plt.figure(figsize=figsize)
+    try:
+        heatmap = sns.heatmap(df_cm, annot=True, fmt="d")
+    except ValueError:
+        raise ValueError("Confusion matrix values must be integers.")
+    heatmap.yaxis.set_ticklabels(heatmap.yaxis.get_ticklabels(), rotation=0, ha='right', fontsize=fontsize)
+    heatmap.xaxis.set_ticklabels(heatmap.xaxis.get_ticklabels(), rotation=45, ha='right', fontsize=fontsize)
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    return fig
